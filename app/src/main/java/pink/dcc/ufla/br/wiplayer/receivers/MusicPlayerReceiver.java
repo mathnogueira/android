696413ab7782cf9e8ea43b6c19ba9719328874c3
Matheus Barba <matheus.barba.lemaf@gmail.com>
2017-08-12 20:19:33 -0300

    Easter egg

---
+++ app/src/main/java/pink/dcc/ufla/br/wiplayer/receivers/MusicPlayerReceiver.java
@@ -0,0 +1,32 @@
+package pink.dcc.ufla.br.wiplayer.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import pink.dcc.ufla.br.wiplayer.R;
+
+/**
+ * Created by barba on 8/12/17.
+ */
+
+public class MusicPlayerReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(300) + 300;
+        Handler handler = new Handler();
+
+        Toast.makeText(context, ""+randomNumber, Toast.LENGTH_SHORT).show();
+        handler.postDelayed(() -> {
+            MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.splash);
+            mediaPlayer.start();
+        }, randomNumber * 1000);
+    }
+}
