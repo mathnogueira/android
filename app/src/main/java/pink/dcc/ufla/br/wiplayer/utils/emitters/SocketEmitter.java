fe2136700cfe7c6163a551ac5fbe5c3f16a6fa10
Rafael <rafa.ufla@gmail.com>
2017-08-13 18:54:54 -0300

    recebendo json de músicas e tocando

---
+++ app/src/main/java/pink/dcc/ufla/br/wiplayer/utils/emitters/SocketEmitter.java
@@ -0,0 +1,11 @@
+package pink.dcc.ufla.br.wiplayer.utils.emitters;
+
+import com.github.nkzawa.emitter.Emitter;
+/**
+ * Created by rafael on 13/08/17.
+ */
+
+public class SocketEmmiter {
+
+    public static Emitter
+}
